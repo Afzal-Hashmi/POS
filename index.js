daa105522b8a4e38f3693ced492fc2de1f78a33d
Afzal-Hashmi <afzalhamid971@gmail.com>
2024-07-29 06:14:05 +0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const cookieparser = require("cookie-parser");
 const { connectDB } = require("./connections/db");
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(cookieparser());
 
 // mapping urls to specfic routes
